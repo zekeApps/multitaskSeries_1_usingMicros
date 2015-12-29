ce4d831f1f60158ea10f350d9dcaeef85d20f769
zekeApps <sheqel@live.com>
2015-12-29 16:37:36 -0600

    adding Class Blinker , this way the code is not repeated

--- multitaskSeries_1_usingMicros/multitaskSeries_1_usingMicros.ino
+++ multitaskSeries_1_usingMicros/multitaskSeries_1_usingMicros.ino
@@ -3,29 +3,58 @@
  Created:	12/29/2015 3:59:13 PM
  Author:	gonzaleze
 */
-uint8_t eLED = 13;
-uint8_t LEDstate = LOW;
-uint32_t prevMillis = 0;
-uint32_t tON = 10;
-uint32_t tOFF = 300;
+
+
+
+
+
+class Blinker {
+	// Class Member Variables
+	// Thses are initialized a startup
+	uint8_t eLED = 13;
+	uint32_t tON = 10;
+	uint32_t tOFF = 300;
+
+	// These maintin the current State
+	uint8_t LEDstate = LOW;
+	uint32_t prevMillis = 0;
+
+public:
+	Blinker(uint8_t pin, uint32_t on, uint32_t off) {
+		eLED = pin;
+		pinMode(eLED, OUTPUT);
+		tON = on;
+		tOFF = off;
+
+		LEDstate = LOW;
+		prevMillis = 0;
+	}
+
+	void Update() {
+		unsigned long currMillis = millis();
+		if ((LEDstate) && (currMillis - prevMillis >= tON)) {
+			LEDstate = LOW;
+			prevMillis = currMillis;
+			digitalWrite(eLED, LEDstate);
+		}
+		else if ((LEDstate == LOW) && (currMillis - prevMillis >= tOFF)) {
+			LEDstate = HIGH;
+			prevMillis = currMillis;
+			digitalWrite(eLED, LEDstate);
+		}
+	}
+};
+
+Blinker led1(13, 10, 400);
+Blinker led2(12, 500, 500);
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-	pinMode(eLED, OUTPUT);
+
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-	unsigned long currMillis = millis();
-
-	if ((LEDstate) && (currMillis - prevMillis >= tON)) {
-		LEDstate = LOW;
-		prevMillis = currMillis;
-		digitalWrite(eLED, LEDstate);
-	}
-	else if ((LEDstate == LOW) && (currMillis - prevMillis >= tOFF)) {
-		LEDstate = HIGH;
-		prevMillis = currMillis;
-		digitalWrite(eLED, LEDstate);
-	}
+	led1.Update();
+	led2.Update();
 }
